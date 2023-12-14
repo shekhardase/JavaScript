73e1d66320d7b983e09b07c4804c20d954beff2e
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-14 16:53:27 +0000

    Variables Introduction

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,27 @@
+const AccountId = 12123123  //this cannnot be changed
+
+let AccountEmail = " gmail.com "   
+
+var AccountPassword = " Kakashi "
+
+AccountCity =  " Latur "
+
+
+// AccountId = 2   //Not allowed
+AccountEmail = "email.com"
+
+AccountPassword = " Ninja "
+
+AccountCity = "Nanded"
+
+let AccountPrice
+
+console.log(AccountId);
+
+console.table([AccountId , AccountEmail , AccountPassword , AccountCity ,AccountPrice])
+
+
+/* 
+Preferr not to use var 
+Because of issue in block scope and function scope 
+*/
