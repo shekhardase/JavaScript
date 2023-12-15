8e3b99c35b48104b5bd5c41dfb3be909922ba299
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-15 16:24:37 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,41 @@
+"use strict"; //treat all js code as newer version
+//above is command This will treat code in new version 
+//no need bydefault strict
+
+
+// alert(3+3)  //we are using node js not browser
+
+// (; is avoided by user)
+
+console.log(3 - 1)
+console.log("Shekhar")
+
+let name = "shekhar"    //string
+
+let age = 121    //int
+
+let IsLoggedIn = false    //bool
+let state;   //undefined 
+//null is empty representation
+//number => 2 to power 53  
+//bigint
+//string => " "
+// boolean => true / false
+// null => stand alone value
+//undefined => 
+
+//symbol  define individual componends  (unique)
+
+//above are primitive datatypes
+
+
+// now Object
+
+console.log(typeof "Hello")  //stirng
+console.log(typeof undefined)  //undefined
+
+console.log(typeof null)   //object
+
+
+
+
