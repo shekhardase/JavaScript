9a61aa3ab11e28015ad6e9cda444c8853fb854e7
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-16 18:07:44 +0000

    Datatype checking

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -1,3 +1,4 @@
+/*
 let score = "10"
 // let score = null
 // let score = undefined 
@@ -32,4 +33,36 @@ console.log(typeof someNumber)
 let stringnumber = String(someNumber)
 
 console.log(stringnumber);
-console.log(typeof stringnumber)
+console.log(typeof stringnumber) */
+
+// *************** OPERATIONS ************** //
+
+// let value = 3 ;
+// let negvalue = - value ;
+// console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);   //power
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1 = "hello";
+// let str2 = " shekhar";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+
+// num1 = num2 = num3 = 2+23
+
