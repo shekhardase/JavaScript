9c45082652fc5310cb4b3bef57ba44878e0425c9
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-16 18:07:55 +0000

    Comparision of datatypes

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);     //js automatically convert string to no
+
+console.log(null>0);   //false
+console.log(null==0);  //false
+console.log(null>=0);  //true
+
+//same result for undefined
+// but for all condition false
+
+// === strict check     // check datatype also
+
+console.log("2" == 2);  // true
+console.log("2" === 2)   // false
