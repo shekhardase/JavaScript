7a43601a2c12513e699ec4f9ddd53fc811274c68
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-19 13:09:51 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,50 @@
+// const name = "shekhar"
+// const repocount = 4
+
+// // console.log(name + repocount + " VALUE ");
+
+
+// console.log('Hello my name is ${name} and my repo count is ${repocount}');
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+
+// const gameName = new String("PubG");
+
+// // here stings are objects not arrays
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);   // length is a property not a method
+// console.log(gameName.toUpperCase());  // toUpperCase is a method not a property
+
+// console.log(gameName);
+// console.log(gameName.charAt(3));
+
+// console.log(gameName.indexOf('G'));
+// console.log(gameName.substring(0,3));
+// console.log(gameName.slice(0,3));
+
+// console.log(gameName.split(''));
+
+// const NewString = "    Hello World    ";
+
+// console.log(NewString.trim());
+// console.log(NewString);
+
+// const url = "https://www.google.com/search?q=javascript+string+methods&oq=javascript+string+methods&aqs=chrome..69i57j0l7.10559j0j7&sourceid=chrome&ie=UTF-8";
+// console.log(url.replace('%20' , '-'));
+
+// console.log(url);
+
+url.includes('google');
+console.log(url.includes('google'));
+
+
+//string to array
+const myString = "shekhar";
+// console.log(Array.from(myString));
+
+
+//using split
+console.log(myString.split(''));
