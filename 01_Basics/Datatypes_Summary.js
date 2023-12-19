005d1f44e475b26dbf7de237d9d88f459fd05685
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-19 12:45:19 +0000

    Heap and Stack memory

--- 01_Basics/Datatypes_Summary.js
+++ 01_Basics/Datatypes_Summary.js
@@ -1,39 +1,63 @@
-// type
+// // type
 
-// primitive
-//  |
-// 7 typess  : string , number, boolean , null , undefined , symbol , BigInt
+// // primitive
+// //  |
+// // 7 typess  : string , number, boolean , null , undefined , symbol , BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
-
+// // const bigNumber = 3456543576654356754n
 
 
 
-//Reference type (non primitive)
-// Array , objects , functions
 
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// //Reference type (non primitive)
+// // Array , objects , functions
 
-const myFunction = function(){
-    console.log("Hello world");
-}
 
-console.log(typeof anotherId);
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+
+// // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++==
+
+// // two types of memoery stack and heap
+// // stack used in primitive      gives copy
+// // heap used in non primitive   gives reference
+
+// let MyYoutubeChannel = "shekhardase"
+// let anothername = MyYoutubeChannel;
+// console.log(anothername);
+
+// let userOne = {
+//     email:"user@google.com",
+//     upi:"uerba"
+// }
+
+// let userTwo = userOne
+
+// userTwo.email = "hiteshChaudhary";
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
