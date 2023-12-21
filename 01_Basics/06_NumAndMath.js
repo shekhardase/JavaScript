7a0cf1ffb53f7478bf95d67aa2606aa835610580
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-21 19:01:32 +0000

    Math Numbers

---
+++ 01_Basics/06_NumAndMath.js
@@ -0,0 +1,41 @@
+// const score = 100
+// console.log(score);
+
+// const balence = new Number(1001.21981);
+// console.log(balence)
+
+// console.log(balence.toString.length);
+// console.log(balence.toFixed(1));
+
+// const otherNumber = 112.2313;
+// console.log(otherNumber.toPrecision(5));
+
+// const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// // ===========================//==========================
+
+// ///maths
+
+// //comes by default
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.3));
+// console.log(Math.ceil(3.7));
+
+
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random() * 10) +1);
+
+const min = 10;
+const max= 20;
+
+// Math.random() * (max - min);
+console.log(Math.floor(Math.random()) *(max - min +1))
+
+
+
+
