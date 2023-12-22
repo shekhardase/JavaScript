0c95e6b94af9514dc3afc08f7a58f3f7492de9e1
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-22 18:45:26 +0000

    Date and time

---
+++ 01_Basics/07_Date_and_Time.js
@@ -0,0 +1,17 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.getDay());
+console.log(myDate.getDate());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getUTCMilliseconds());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toJSON());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date()
+
+console.log(Date())
