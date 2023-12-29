28f59f4f0d14a01160e408ee78bd65386718f87f
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-29 07:05:17 +0000

    Objects in Javascript

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,49 @@
+// // SingleTon
+
+
+// // Object Literals
+
+
+// Object.create             // constructor method
+const JsUser = {
+    name:"shekhar",        //key can be anything
+    age:19,
+    location: "pune"    ,
+    email: "fodsin@io",
+    isLogIn: false,     
+    LastLogIn : ["monday" , "Friday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"]);    //by using square notation we can access deep object within key
+
+// console.log(typeof JsUser["email"])    // string
+// console.log(typeof JsUser["age"])          //  number 
+//                                         // inorder to access we use " "  as it treats as string
+
+
+// JsUser.email = "zorop@gmail.com"
+
+// Object.freeze(JsUser)          // this will prevent chagning Object 
+
+// console.log(JsUser["email"])
+
+
+
+
+
+// // myArray = ["h" , "i"]
+
+// // myArray[1]
+
+
+
+JsUser.greeting = function()
+{
+    console.log("Hello Js User");
+}
+
+console.log(JsUser.greeting());
+
+
+
