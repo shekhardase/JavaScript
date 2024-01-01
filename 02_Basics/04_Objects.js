5c2d9fb3deb68a999d3bc9d71897f5129801e2de
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-01 13:35:21 +0000

    Objects_Complete

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,93 +1,127 @@
-//  singleton objects here
+// //  singleton objects here
 
 
-// const instapp = new Object()
-// ----------or---------------
+// // const instapp = new Object()
+// // ----------or---------------
 
-instapp = {}
-instapp.id = "1234abc"
-instapp.name = "Sam"
-instapp.isLoggedIn = true
-
-
-const regularUser = {             //multiple nesting is possible 
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "shekhar",
-            lastname: " Dase "
-        }
-    }
-}
+// instapp = {}
+// instapp.id = "1234abc"
+// instapp.name = "Sam"
+// instapp.isLoggedIn = true
 
 
-// console.log(regularUser.fullname.userfullname.firstname);     //nesting can be accessed using dots
-
-const obj1 = { 1: "a", 2: "b" }
-const obj2 = { 3: "a", 4: "b" }
-const obj4 = { 6: "a", 8: "b" }
-// const obj3 = {obj1 , obj2};        //objects in objects     //we face problem here because of nesting
-
-// const obj3 = Object.assign( {} ,obj1 , obj2 , obj4);
-//              empty target           all other source
-
-//     const returntarget  = object.assign(target , source);
+// const regularUser = {             //multiple nesting is possible 
+//     email: "some@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "shekhar",
+//             lastname: " Dase "
+//         }
+//     }
+// }
 
 
-const obj3 = { ...obj1, ...obj2 }          //spread and print
-console.log(obj3);
+// // console.log(regularUser.fullname.userfullname.firstname);     //nesting can be accessed using dots
+
+// const obj1 = { 1: "a", 2: "b" }
+// const obj2 = { 3: "a", 4: "b" }
+// const obj4 = { 6: "a", 8: "b" }
+// // const obj3 = {obj1 , obj2};        //objects in objects     //we face problem here because of nesting
+
+// // const obj3 = Object.assign( {} ,obj1 , obj2 , obj4);
+// //              empty target           all other source
+
+// //     const returntarget  = object.assign(target , source);
 
 
-// console.log(instapp)
+// const obj3 = { ...obj1, ...obj2 }          //spread and print
+// console.log(obj3);
 
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-]
+// // console.log(instapp)
 
-users[1].email
-// console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// users[1].email
+// // console.log(tinderUser);
+
+// // console.log(Object.keys(tinderUser));
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+// // course.courseInstructor
+
+// const { courseInstructor: instructor } = course
+
+// // console.log(courseInstructor);
+// console.log(instructor);
+
+// // {
+// //     "name": "hitesh",
+// //     "coursename": "js in hindi",
+// //     "price": "free"
+// // }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
+//================================================================================
 
 
 const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "hitesh"
+    coursename : "js in hindi",
+    price : "1000",
+    courseInstructor : "shekhar",
 }
 
 // course.courseInstructor
 
-const { courseInstructor: instructor } = course
+const {courseInstructor : innn} = course
+// console.log(courseInstructor) 
 
-// console.log(courseInstructor);
-console.log(instructor);
+console.log(innn);
 
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
+// const navbar = ({company})  => {
+
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// navbar( company = "shekhar")
 
+
+
+{
+
+    //json
+
+    // name: "shekhar",
+    // course:"JS";
+
+}
