ce2cdaacb0e69a0575cff03f3ab5dceb9275b503
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-02 09:04:06 +0000

    javascript function

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,58 @@
+
+
+function Name() {
+    console.log("J")
+    console.log("S")
+    console.log("O")
+    console.log("N")
+
+}
+
+Name();
+
+
+// function Add(N1 , N2)
+// {
+//     console.log(N1+N2);
+// }
+// Add(4,3);             // mumber
+// Add(4,"3");           // number + string
+// Add("4",3);           // number + string
+// Add("4","3");         // string + string
+
+// Add(4 , null);        // number + nothing
+
+
+// const result = Add(5,5);
+
+// console.log("result :" , result);    // it should store but prints undefined
+
+
+function Add(number1, number2) {
+    // let result = number1 + number2;
+    // return result;
+
+    return number1 + number2
+}
+
+// const newresult = Add(3, 3)
+// Add(3,3)          will not print
+
+// console.log("Result : ", newresult)
+
+
+
+function loginusermessage(username = "Sam")                //after = is default value
+{
+    // if(username == undefined)            
+    if(!username)
+    {
+        console.log("Please enter username")
+        return 
+    }
+    return `${username} just logged in `
+}
+
+console.log(loginusermessage("shekhar"))
+console.log(loginusermessage(""))
+console.log(loginusermessage());
