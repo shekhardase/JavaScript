896d44abae4209fb2315d01906db818b172502c5
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-03 17:23:12 +0000

    Functions using different parameters

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -1,58 +1,94 @@
 
 
-function Name() {
-    console.log("J")
-    console.log("S")
-    console.log("O")
-    console.log("N")
+// // function Name() {
+// //     console.log("J")
+// //     console.log("S")
+// //     console.log("O")
+// //     console.log("N")
 
-}
+// // }
 
-Name();
+// // Name();
 
 
-// function Add(N1 , N2)
+// // // function Add(N1 , N2)
+// // // {
+// // //     console.log(N1+N2);
+// // // }
+// // // Add(4,3);             // mumber
+// // // Add(4,"3");           // number + string
+// // // Add("4",3);           // number + string
+// // // Add("4","3");         // string + string
+
+// // // Add(4 , null);        // number + nothing
+
+
+// // // const result = Add(5,5);
+
+// // // console.log("result :" , result);    // it should store but prints undefined
+
+
+// // function Add(number1, number2) {
+// //     // let result = number1 + number2;
+// //     // return result;
+
+// //     return number1 + number2
+// // }
+
+// // // const newresult = Add(3, 3)
+// // // Add(3,3)          will not print
+
+// // // console.log("Result : ", newresult)
+
+
+
+// // function loginusermessage(username = "Sam")                //after = is default value
+// // {
+// //     // if(username == undefined)            
+// //     if(!username)
+// //     {
+// //         console.log("Please enter username")
+// //         return 
+// //     }
+// //     return `${username} just logged in `
+// // }
+
+// // console.log(loginusermessage("shekhar"))
+// // console.log(loginusermessage(""))
+// // console.log(loginusermessage());
+
+
+// // -------------------------------------------------------------------------------------
+
+// function calculateCartPrice(val1 , val2 , ...num1)             // ... is rest operator
 // {
-//     console.log(N1+N2);
+//     return num1;       //will return array now because of rest operator
 // }
-// Add(4,3);             // mumber
-// Add(4,"3");           // number + string
-// Add("4",3);           // number + string
-// Add("4","3");         // string + string
 
-// Add(4 , null);        // number + nothing
+// console.log(calculateCartPrice(1,2,4))                 //to handle this rest operator is used
 
+// const user = {
+//     username:"hitesh",
+//     price:100
+// }
 
-// const result = Add(5,5);
+// function handleObject(anyobject)
+// {
+//     console.log(`username is ${anyobject.username} and price is $
+//     {anyobject.price}`)
+// }
 
-// console.log("result :" , result);    // it should store but prints undefined
-
-
-function Add(number1, number2) {
-    // let result = number1 + number2;
-    // return result;
-
-    return number1 + number2
-}
-
-// const newresult = Add(3, 3)
-// Add(3,3)          will not print
-
-// console.log("Result : ", newresult)
+// handleObject({
+//     username:"sam",
+//     price:121
+// })
 
 
-
-function loginusermessage(username = "Sam")                //after = is default value
+const mynewarray = [1,2,3,4,4]
+function returnSecondValue(getArray)
 {
-    // if(username == undefined)            
-    if(!username)
-    {
-        console.log("Please enter username")
-        return 
-    }
-    return `${username} just logged in `
+    return getArray[1]
 }
 
-console.log(loginusermessage("shekhar"))
-console.log(loginusermessage(""))
-console.log(loginusermessage());
+console.log(returnSecondValue(mynewarray));
+console.log(returnSecondValue([1,2,3,4,4]));
