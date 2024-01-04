d5a7d999e8575a479a96d0e8451e4dabee4c4f15
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-04 11:03:55 +0000

    Scope in Javascript

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,23 @@
+
+let a   = 100
+
+
+if(true)
+{
+    let a = 10             //scope limited
+    const b = 20           //scope limited
+    // var c = 40 //or             //ye baher ayega
+    c =40
+}
+
+
+console.log(a)
+// console.log(b)
+console.log(c)
+
+
+
+
+
+
+
