15f70b4f62ae84fb13484707c88daa964f9f8850
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-06 15:22:00 +0000

    more about scopes

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -1,23 +1,70 @@
 
-let a   = 100
+// // let a   = 100
 
 
-if(true)
+// // if(true)
+// // {
+// //     let a = 10             //scope limited
+// //     const b = 20           //scope limited
+// //     // var c = 40 //or             //ye baher ayega
+// //     c =40
+// // }
+
+
+// // console.log(a)
+// // // console.log(b)
+// // console.log(c)
+
+
+
+// // -----------------------------------------------------------------
+
+// // function one() {
+// //     const usernmae  = " shekhar "
+
+
+// //     function two() {
+// //         const website = " youtube "
+// //         console.log(usernmae);
+// //     }
+
+// //     // console.log(website)              nhi chalega
+
+
+// //     two()
+// // }
+
+// // one()
+
+// if(true)
+// {
+//     const usernmae = " shekhar "
+//     if(usernmae == " shekhar ")
+//     {
+//         const website = "youtube"
+//         console.log(usernmae+website)
+//     }
+//     // console.log(website)             error not accessable
+// }
+
+// // console.log(usernmae)
+
+
+// ===============interesting==========================
+
+
+function addone(num)
 {
-    let a = 10             //scope limited
-    const b = 20           //scope limited
-    // var c = 40 //or             //ye baher ayega
-    c =40
+    return num+1;
 }
 
 
-console.log(a)
-// console.log(b)
-console.log(c)
+// addone(5)           // will not print   just call not print command
+console.log(addone(5))
 
 
-
-
-
-
-
+addtwo(5)                        // error later discussed in hoisting 
+const addtwo = function(num)
+{
+    return num+2;
+}
