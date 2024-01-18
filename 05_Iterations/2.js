8d584fb04c4e09786181ead747d97a430523de92
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-01-18 16:47:04 +0000

    loops

---
+++ 05_Iterations/2.js
@@ -0,0 +1,22 @@
+let index = 1;
+while (index <= 0) {
+    console.log('value of index' `${index}`);
+    index = index + 2;
+
+}
+
+let myArray = ['flash ', 'batman', 'superman']
+let arr = 0;
+
+while (arr < myArray.length) {
+    console.log(`print ${myArray}`);
+    arr = arr + 1;
+
+}
+
+let score =1 ; 
+
+do {
+    console.log(`score is ${ score }`);
+    score = score + 1;
+} while (score <= 10)
