a9810849c958aba7d3c0b76a95dbec7c1568f910
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-17 00:47:40 +0530

    operators revision

---
+++ JS Recap/Chapter_1/script6.js
@@ -0,0 +1,41 @@
+// Operators in javascript 
+
+
+// arithmetic operators
+console.log("Operators in js ")
+let a = 13;
+let b = 12;
+
+console.log("a + b", a + b);
+console.log("a - b", a - b);
+console.log("a / b", a / b);
+console.log("a * b", a * b);
+console.log("a % b", a % b);
+
+console.log(a);  // 13
+console.log(++a);  // first incresed then printed 14 
+console.log(a++);   // 14 print first later increased to 15
+// a;
+
+console.log(a);      // 15
+a--;                // prints 14 later decresed to 13
+console.log(a);      
+
+
+// assignment operators
+console.log("--------------------------------------------------------");
+let l =  5;
+l = l +5 
+console.log(l);
+
+
+// __________________________
+// comparsion operators 
+
+let comp1 = 3;
+let comp4 = 23;
+
+console.log(comp1 == comp4);
+
+
+// === checks type 
