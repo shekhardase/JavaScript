871ea1117a43dcabe093189b1a3e810b23e0ad1f
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-17 18:00:35 +0530

    practice set

---
+++ JS Recap/Chapter_1/PracticeSet.js
@@ -0,0 +1,43 @@
+// // Q 1 
+
+// let number = 23;
+
+// if (number > 20) {
+//     console.log("Out of bounds");
+// }
+
+// else if (number < 10) {
+//     console.log("number is too low");
+// }
+
+// else if (number > 10 && number < 20) {
+//     console.log("perfect");
+// }
+
+
+
+// Q 2 
+
+// let number = 20;
+
+// switch (number) {
+//     case 20:
+//         console.log('gay'); 
+//         break;
+
+//     default:
+//         console.log('still gay');
+//         break;
+// }
+
+
+// let number = 10;
+
+// if (number % 2 == 0 || number & 3 == 0) {
+//     console.log('divisible by 2 or 3');
+//  }
+
+
+// let age = 20
+
+// age > 18  ? "You can drive" : "You cannot drive"
