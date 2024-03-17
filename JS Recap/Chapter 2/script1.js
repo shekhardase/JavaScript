67de91cafb1796921e7a652e62136b436b9c8a22
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-17 18:32:15 +0530

    Chapt 2 started

---
+++ JS Recap/Chapter 2/script1.js
@@ -0,0 +1,33 @@
+// for (let index = 0; index < 30; index++) {
+//     // const element = array[index];
+//     console.log(index);
+// }
+
+
+
+// let sum = 0 ;
+// let  n = prompt("Enter the number")
+// n= Number.parseInt(n)
+// for(let i  = 0 ; i<n ; i++)
+// {
+//     sum = sum + i ;
+// }
+
+// let obj = {
+//     name: "John",
+//     age: 25,
+//     city: "New York",
+//     occupation: "Developer",
+//     hobbies: ["coding", "reading", "gaming"]
+// };
+
+
+// for (let i in obj) {
+//    console.log(" data " + i + " is " + obj[i]);
+// }
+
+
+// for(let a of "Shekhar")
+// {
+//     console.log(a);
+// }
---
+++ JS Recap/Chapter 2/script2.js
@@ -0,0 +1,15 @@
+// let a = prompt("enter the number")
+// // let a = 10;
+// a = Number.parseInt(a)
+
+// while (a>0) {
+//     console.log('gay');
+//     a--;
+// }
+
+
+let n = 10;
+do {
+    console.log("Gay");
+    n--;
+} while (n>0);
