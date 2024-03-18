acca163246df74afa7340f241ee017243b47d9fe
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-18 16:27:38 +0530

    Practice set on Strings

---
+++ JS Recap/Chapter_3/practice.js
@@ -0,0 +1,29 @@
+console.log("har\"".length);
+
+
+// includes 
+
+let s1 = " Niggu "
+let s2 = " "
+
+console.log(s1.includes(s2));      // blank space
+
+console.log(s1.startsWith(' '));    // blank space contains in s1 
+
+console.log(s1.endsWith(' '));    // blank space contains at end of s1
+
+
+
+let cow = 'GAYYA'
+
+console.log(cow.toLowerCase());
+
+
+let sen = " Please give me Rs. 1000 "
+
+let num = sen.indexOf("1000");
+console.log(num);
+console.log(sen.charAt(num));
+
+console.log(sen.slice(20));
+let ch = "what you doing "
