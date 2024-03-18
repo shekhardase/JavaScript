1a2da8ec3f4a8d8e660a4fee12c09ba6313e2605
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-18 14:08:48 +0530

    practice set on loops and functions

--- JS Recap/Chapter_2/practice.js
+++ JS Recap/Chapter_2/practice.js
@@ -11,10 +11,47 @@ const marks = {
     subject5: Math.floor(Math.random() * 101)
 };
 
-console.log(marks);
 
-
-
-for (let mark in marks) {
-    console.log(marks[mark]);
+for (let i in marks) {
+    console.log(i, marks[i]);
 }
+
+// console.log(marks);
+
+
+
+// for (let mark in marks) {
+//     console.log(marks[mark]);
+// }
+
+// for (let index = 0; index < marks.length; index++) {
+// console.log(marks.Object.keys(marks)[index]);
+// console.log(Object.keys(marks)[index]);
+// }
+
+
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//     console.log(Object.keys(marks)[i], " ", marks[Object.keys(marks)[i]]);
+// }
+
+
+// q3 
+
+// let number = 109;
+
+// let i
+
+// while (number != i) {
+//     i = prompt("enter a number");
+// }
+
+// console.log("Entered number is correct");
+
+
+// find mean
+
+let mean = (a, b, c, d) => {
+    console.log((a + b + c + d) / 4);
+}
+
+mean(1, 1, 1, 1)
---
+++ JS Recap/Chapter_2/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let number = 109;
+
+// let i
+
+// while (number != i) {
+//     i = prompt("enter a number");
+// }
+
+// console.log("Entered number is correct");
+
