b88344689fc4bc463bbd5fc50eb2c89ca4679242
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-22 12:01:18 +0530

    Practice questions on arrays using map , filter and reduce

---
+++ JS Recap/Chapter_5/practice.js
@@ -0,0 +1,73 @@
+
+// // let a = []
+
+// // for(let i = 0; i < a.length;i++)
+// // {
+// //  a[i] = prompt("enter the number " )
+// // }
+
+// // for (let i = 0; i < a.length; i++) {
+// //     const element = a[i];
+// //     console.log(element);
+// // }
+
+
+// // let a = [];
+// // let input = prompt("Enter a number or type 'done' to finish:");
+
+// // while (input !== 'done') {
+// //     a.push(input);
+// //     input = prompt("Enter a number or type 'done' to finish:");
+// // }
+
+// // for (let i = 0; i < a.length; i++) {
+// //     console.log(a[i]);
+// // }
+
+
+// // let numInputs = parseInt(prompt("Enter the number of inputs: "));
+// // let a = [];
+
+// // for(let i = 0; i < numInputs; i++) {
+// //     a[i] = prompt("Enter number " + (i + 1));
+// // }
+
+// // for (let i = 0; i < a.length; i++) {
+// //     const element = a[i];
+// //     console.log(element);
+// // }
+
+
+
+
+// // numbers divisible by 7 only
+
+// let arr = [1,2,3,4,5,6,7,8,9 , 10 , 20 , 30 , 50 , 100]
+// let arr2 = arr.filter((a) =>{
+//     return a%10 == 0 ;
+// })
+
+// console.log(arr2);
+
+
+
+// // squares of element using map function
+
+// let ouch = [1,2,3,4,5,6,7,8,9,10]
+
+// let newArray = ouch.map((i)=>{
+//     return i * i ;
+// })
+
+// console.log(newArray);
+
+
+// let fac = [ 1,2,3,4,5]
+// let i ; 
+// let j = i-1;
+// let fact = (i , j )=>{
+//     return i * j
+// }
+
+// let newarr = fac.reduce(fact)
+// console.log(newarr);
