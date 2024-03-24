67e3754b1b1ca9216335550aeb99414934d2a3fc
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-24 21:23:47 +0530

    console messages and prompts

---
+++ JS Recap/Chapter_6/script3.js
@@ -0,0 +1,21 @@
+alert("AYOOOOOOO NIGGAA")
+
+
+let a = prompt("enter value of a ")
+
+document.write(a);
+
+alert("You entered a of type " + ( typeof a ) )
+
+let write = confirm("u want to write your name ???")
+
+if(write)
+{
+    document.write(a);
+}
+else{
+    document.write("khikhikhi");
+}
+
+document.write(a);
+
