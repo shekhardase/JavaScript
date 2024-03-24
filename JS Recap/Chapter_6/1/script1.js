50546ab03f3931bd3b00b59e2a4f6f7429260a61
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-24 20:06:35 +0530

    console log methods

---
+++ JS Recap/Chapter_6/1/script1.js
@@ -0,0 +1,26 @@
+console.log('helo');
+
+// multiple methods are available for console log
+
+console.error("Wrongg");
+
+
+console.assert(5 > 4);      // shows wrong or right 
+// if false assertion fails
+
+console.clear();       // clear console
+
+//write in table format
+obj = {1:1 , 2 : 2 , 3 : 3 , 4:4}
+console.table(obj);
+
+
+
+//warning
+console.warn("please dont try codingg --- ");
+
+
+console.info("this is an information");
+
+
+console.table(console);
