daa0698f9e7c62b6afb35da995cba3b7be3270b9
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-25 14:09:39 +0530

    DOM BOM introduction

---
+++ JS Recap/Chapter_6/2/script4.js
@@ -0,0 +1,7 @@
+window.console.log(window)
+console.log(document.body);
+document.body.style.background = "red";
+
+alert("Haarrr");
+
+location.href = "https://www.youtube.com/watch?v=xOCzjgjedRc&list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR&index=28"
